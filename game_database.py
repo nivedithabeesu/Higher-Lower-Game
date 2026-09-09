64f2f0f0236d6b1101b6ebd2443c01f7adea2292
nivedithabeesu <niveditha.beesu5@gmail.com>
2026-09-09 20:30:43 +0530

    Add files via upload

---
+++ game_database.py
@@ -0,0 +1,128 @@
+data=[
+    {
+        'name':"jenny's lectures",
+        'follower_count':1,
+        'description':'youtuber',
+        'country':'india'
+
+    },
+    {
+        'name':"cristiano ronaldo",
+        'follower_count':600,
+        'description':'footballer',
+        'country':'portugal'
+
+    },
+    {
+        'name':"narendra modi",
+        'follower_count':90,
+        'description':'prime minister',
+        'country':'india'
+
+    },
+    {
+        'name':"fit tuber",
+        'follower_count':7,
+        'description':'youtuber',
+        'country':'india'
+
+    },
+    {
+        'name':"salena gomez",
+        'follower_count':420,
+        'description':'musician and actress',
+        'country':'us'
+
+    },
+    {
+        'name':"kylie jenner",
+        'follower_count':400,
+        'description':'reality tv personality and businesswoman',
+        'country':'us'
+
+    },
+    {
+        'name':"pankaj tripathi",
+        'follower_count':5,
+        'description':'actor',
+        'country':'india'
+
+    },
+    {
+        'name':"lionel messi",
+        'follower_count':480,
+        'description':'footballer',
+        'country':'argentina'
+
+    },
+    {
+        'name':"beyonce",
+        'follower_count':315,
+        'description':'musician',
+        'country':'us'
+
+    },
+    {
+        'name':"t_series",
+        'follower_count':245,
+        'description':'music label and movie studio',
+        'country':'india'
+
+    },
+    {
+        'name':"neeraj chopra",
+        'follower_count':6,
+        'description':'athlete',
+        'country':'india'
+
+    },
+    {
+        'name':"justin biebar",
+        'follower_count':243,
+        'description':'musician',
+        'country':'canada'
+
+    },
+    {
+        'name':"taylor swift",
+        'follower_count':345,
+        'description':'musician',
+        'country':'us'
+
+    },
+    {
+        'name':"jennifier lopez",
+        'follower_count':249,
+        'description':'musician and actress',
+        'country':'us'
+
+    },
+    {
+        'name':"shaira",
+        'follower_count':98,
+        'description':'musician',
+        'country':'colombia'
+
+    },
+    {
+        'name':"isro",
+        'follower_count':1,
+        'description':'space agency',
+        'country':'india'
+
+    },
+    {
+        'name':"virat kholi",
+        'follower_count':234,
+        'description':'cricketer',
+        'country':'india'
+
+    },
+    {
+        'name':"nasa",
+        'follower_count':45,
+        'description':'space agency',
+        'country':'us'
+
+    },
+]
